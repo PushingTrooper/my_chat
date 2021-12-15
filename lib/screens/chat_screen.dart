5f64ee858624e521428390425d681480210e85a5
alamiju <junis.milori@raiffeisen.al>
2021-12-15 12:00:21 +0100

    Added firebase messaging and tested some stuff

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,34 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission();
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      print(message);
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      final notification = message.notification;
+      print(message);
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
